a4580f4bdb31f1ad7c71b8d5967ed48ae47df628
sono0902 <149942287+sono0902@users.noreply.github.com>
2024-01-08 02:05:17 +0900

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,21 @@
+$(function() {
+  // 文字色変化
+  $("#change-color").on("click", function() {
+    $("#target").css("color", "blue"); 
+  });
+
+  // 文字内容変化
+  $("#change-text").on("click", function() {
+    $("#target").text("おやすみ"); 
+  });
+
+  // フェードアウト
+  $("#fade-out").on("click", function() {
+    $("#target").fadeOut(1000); 
+  });
+
+  // フェードイン
+  $("#fade-in").on("click", function() {
+    $("#target").fadeIn(1000); 
+  });
+});
